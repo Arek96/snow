30d9d71e3150769d8cae292263a35e0489610a6d
Arek96 <arkadiuszosoba@gmail.com>
2017-12-13 13:29:18 +0100

    added owlcarousel and mobile menu

--- js/snow.js
+++ js/snow.js
@@ -3,210 +3,59 @@ function winScr() {
     'use strict';
     console.log("i work");
     var yPos = window.pageYOffset,
-    brandImg = document.getElementById("brandImg"),
+    brandImg = document.getElementsByClassName("navBrand"),
     nav = document.getElementById("nav"),
-    aNav = document.getElementById("aNav"),
+    mobileNav = document.getElementsByClassName("mobileNav")[0],
     i;
      
     if (yPos > 50) {
         i = 0;
-        brandImg.setAttribute("src", "img/logo.png");
+        document.getElementById("brandImg1").setAttribute("src", "img/logo.png");
+        document.getElementById("brandImg2").setAttribute("src", "img/logo.png");
         for(i=0; i<=4; i++){
             document.getElementsByClassName("aNav")[i].style.color = "grey";
             } 
         nav.style.animationName = "navscrl";
         nav.style.animationDuration = "0.5s";
+        mobileNav.style.animationName = "navscrl";
+        mobileNav.style.animationDuration = "0.5s";
         console.log(document.getElementById('nav').style.animationName);
+        document.getElementsByClassName("mobileBtn")[0].style.color = "grey";
      } 
      else { 
          if(nav.style.animationName == "navscrl"){
             i = 0;
             nav.style.animationName = "navscrlbk";
             nav.style.animationDuration = "0.3s";
-            brandImg.setAttribute("src", "img/logo-light.png");
+              mobileNav.style.animationName = "navscrlbk";
+            mobileNav.style.animationDuration = "0.3s";
+            document.getElementById("brandImg1").setAttribute("src", "img/logo-light.png");
+              document.getElementById("brandImg2").setAttribute("src", "img/logo-light.png");
+              document.getElementsByClassName("mobileBtn")[0].style.color = "white";
             for (i=0; i <= 4; i++){
                 document.getElementsByClassName("aNav")[i].style.color = "white";
                 }
          }
          }
      }
-
-{
-function quotesMouseDown(event) {
-    'use strict';
-    var quotes = document.getElementById("quotes"),
-        mDownX = event.pageX,
-        isMouseDown = false,
-        dist = 0,
-        pos = parseFloat(quotes.style.left),
-        firstPos = pos,
-        bodyWidth = parseFloat(window.getComputedStyle(document.body, null).getPropertyValue("width"));
-        quotes.addEventListener("mousemove", quotoesMouseMove);
-        quotes.addEventListener("mouseup",function() {
-            isMouseDown = true;
-            console.log(isMouseDown);
-            
-            quotes.removeEventListener("mousemove", quotoesMouseMove);});
-    console.log(window.getComputedStyle(document.body, null).getPropertyValue("width") + "style");
-    console.log(bodyWidth);
-        function quotoesMouseMove(event) {
-            var mMoveX = event.pageX,
-                distance = mMoveX - mDownX;
-                
-            if (pos === 0){ 
-                dist = distance; 
-                            }
-            if (pos > 0 || pos < 0) {
-                dist = distance + pos; 
-            }
-            quotes.style.left = dist +"px";
-            console.log(quotes.onmouseup);
-            if (pos === 0 && dist > 0) {
-                
-               quotes.onmouseup = function() {limit1();}
-               console.log("lel")
-            }
-            if(distance > 0 && pos !== 0){
-                quotes.onmouseup = function() {quotesAnim();}
-                console.log("nie wiem");
-               
-            }
-            if((-1)*dist > 0 && distance < 0 && dist <! quotes.offsetWidth){
-                quotes.onmouseup = function () { quotesAnimeForwards();}
-                
-                console.log("dramat")
-            }
-            if (dist*(-1) > (quotes.offsetWidth - bodyWidth)){
-                
-                quotes.onmouseup = function() {limitEnd();}
-                console.log("im here");
-                }
-           
-           console.log(dist);
-            
-        }
- 
-            function quotesAnimeForwards(){
-                'use strict'
-                quotes.style.WebkitTransition = "left, 1s";
-                quotes.style.transition = "left, 1s";
-                quotes.style.left = pos - bodyWidth + "px";
-                setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-                
-            }
-            function quotesAnim(){
-                'use strict'
-                quotes.style.WebkitTransition = "left, 1s";
-                quotes.style.transition = "left, 1s";
-                quotes.style.left = pos + bodyWidth +"px";
-                setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-                
-            }
-            function limit1(){
-                'use strict'
-                quotes.style.WebkitTransition = "left, 1s";
-                quotes.style.transition = "left, 1s";
-                quotes.style.left =  " 0px";
-                 setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-            }
-            function limitEnd(){
-                quotes.style.WebkitTransition = "left, 1s";
-                quotes.style.transition = "left, 1s";
-                quotes.style.left = ((-2)*bodyWidth) +"px";
-                 setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-            }
-    
- }
-function butPos(){
-    window.setTimeout(butPosTimeout, 1001);
-    
+function mobileMenu(){
+    document.getElementsByClassName("mobileNavItems")[0].style.display= "flex";
+    document.getElementsByClassName("mobileBtn")[0].style.display= "none";
+    for(var i =0; i < 5; i++){
+    document.getElementsByClassName("navItemMobile")[i].style.display ="block";
     }
-function butPosTimeout(){
-    var pos = parseFloat(document.getElementById("quotes").style.left);  
-    if (pos == 0) buttonResize(1);
-    if (pos < 0 && pos > (-1)*window.innerWidth ) buttonResize(2);
-    if (pos < (-1)*window.innerWidth && pos> (-2)*window.innerWidth) buttonResize(3);
-    return pos;
+    document.body.style.overflow = "hidden";
+    document.getElementsByClassName("mobileNavExitBtn")[0].style.display= "block";
 }
-function button1Anim(){
-   var quotes = document.getElementById("quotes"),
-       quoutesPos = parseFloat(quotes.style.left),
-       button = document.getElementById("quotesButton1");
-    if(quoutesPos < 0 || quoutesPos > 0){
-        quotes.style.WebkitTransition = "left, 1s";
-        quotes.style.transition = "left, 1s";
-        quotes.style.left =  "0px";
-        buttonResize(1);
-        setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
+function mobileMenuExit(){
+    document.getElementsByClassName("mobileNavItems")[0].style.display= "none";
+    document.getElementsByClassName("mobileBtn")[0].style.display= "block";
+    for(var i =0; i < 5; i++){
+    document.getElementsByClassName("navItemMobile")[i].style.display ="none";
     }
-    
-}
-function button2Anim(){
-   var quotes = document.getElementById("quotes"),
-       quoutesPos = parseFloat(quotes.style.left),
-       bodyWidth = parseFloat(window.getComputedStyle(document.body, null).getPropertyValue("width")),
-       button = document.getElementById("quotesButton2");
-    if(quoutesPos < bodyWidth || quoutesPos > bodyWidth){
-        quotes.style.WebkitTransition = "left, 1s";
-        quotes.style.transition = "left, 1s";
-        quotes.style.left = (-1)*bodyWidth + "px";
-        buttonResize(2);
-        setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-    }
-    
-}
-function button3Anim(){
-    console.log(parseFloat(this));
-   var quotes = document.getElementById("quotes"),
-       quoutesPos = parseFloat(quotes.style.left),
-       bodyWidth = parseFloat(window.getComputedStyle(document.body, null).getPropertyValue("width")),
-       button = document.getElementById("quotesButton3");
-    if(quoutesPos < bodyWidth || quoutesPos > bodyWidth){
-        quotes.style.WebkitTransition = "left, 1s";
-        quotes.style.transition = "left, 1s";
-        quotes.style.left = (-2)*bodyWidth + "px";
-        buttonResize(3);
-        setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-    }
-    
-}
-function resize(){
-    var quotes = document.getElementById("quotes");
-    quotes.style.WebkitTransition = "left, 1s";
-    quotes.style.transition = "left, 1s";
-    quotes.style.left =  " 0px";
-    setTimeout(function(){quotes.style.WebkitTransition = "none";
-                                      quotes.style.transition = "none";
-                                     },1000);
-}
-function buttonResize(whichOne){
-   var activeButton = document.getElementsByClassName("buttonActive")[0],
-       activeButtonIdName = document.getElementsByClassName("buttonActive")[0].id,
-       activeButtonId = document.getElementById(activeButtonIdName);
-    activeButtonId.classList.remove("buttonActive");
-    activeButtonId.classList.add("buttonInActive");
-    document.getElementById("quotesButton"+whichOne).classList.remove("buttonInActive");
-    document.getElementById("quotesButton"+whichOne).classList.add("buttonActive");
-}
+    document.body.style.overflow = "auto";
+    document.getElementsByClassName("mobileNavExitBtn")[0].style.display= "none";
 }
 window.addEventListener('scroll', winScr);
-document.getElementById("quotes").addEventListener("mousedown", quotesMouseDown);
-document.getElementById("quotesButton1").addEventListener("click", button1Anim);
-document.getElementById("quotesButton2").addEventListener("click", button2Anim);
-document.getElementById("quotesButton3").addEventListener("click", button3Anim);
-window.addEventListener("resize", resize);
-document.getElementById("quotes").addEventListener("mouseup", butPos);
+document.getElementsByClassName("mobileBtn")[0].addEventListener("click",mobileMenu);
+document.getElementsByClassName("mobileNavExitBtn")[0].addEventListener("click", mobileMenuExit);
---
+++ js/snowJQuery.js
@@ -0,0 +1,27 @@
+$("doucment").ready(function(){
+    $('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
+    center:true,
+    dots:true,
+        items:1
+    });
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:10,
+    center:true,
+    dots:false,
+    responsiveClass: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+});
